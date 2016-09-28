2c7d9bd2afd96a7f808adc727bb0450362ec5e65
melancholiac <neron655312@gmail.com>
2016-09-28 07:15:42 +0300

    skeleton was born

---
+++ skeleton.py
@@ -0,0 +1,21 @@
+'''
+	This is a skeleton. If u shall pay attention and take care of him,
+	his bones will will be covered by flesh and blood will stream through
+	his veins and arteries. So that he will become alive
+'''
+import vk
+import curses
+import asyncio
+import requests
+upperlevel=\
+{
+'''
+	upperlevel is a selector which sends control to selection choosen.
+'''
+	'My profile':None,
+	'News':None,
+	'Messages':None,
+	'Contacts':None,
+	'Groups':None,
+	'Tracks':None
+}
