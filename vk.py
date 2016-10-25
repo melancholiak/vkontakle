999d298e7b1e0f4f79b1f60cd7b200c636262adf
melancholiac <neron655312@gmail.com>
2016-10-25 20:08:55 +0300

    0

--- vk.py
+++ vk.py
@@ -4,7 +4,7 @@ USER_ID='246194626'
 
 def get_profile(acess_token,user_id,fields):
 	return requests.get('https://api.vk.com/method/users.get?acces_token={0}&user_ids={1}&fields={2}&v=5.57'.format(acess_token,user_id,','.join(fields)))
-def get_p(fields):
+def get_p(*fields):
 	return get_profile(ACESS,USER_ID,fields)		
 
 #r = requests.get('https://api.vk.com/method/messages.getDialogs?PARAMETERS&access_token=2e88d23fa0865126f3dd13914c545af4f516141b2ed13f3fa0e5169078ebe7c15abb7c3673a423052eb72&v=V')
